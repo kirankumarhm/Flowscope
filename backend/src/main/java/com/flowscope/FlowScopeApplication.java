2fb5559f08f273433fe3894b363c8ede4fd4c861
Kiran Kumar H.M <kirankumarhm2004@gmail.com>
2026-07-19 12:17:01 +0530

    Add main application class for FlowScope

---
+++ backend/src/main/java/com/flowscope/FlowScopeApplication.java
@@ -0,0 +1,13 @@
+package com.flowscope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/** FlowScope Web application entry point. */
+@SpringBootApplication
+public class FlowScopeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowScopeApplication.class, args);
+    }
+}
